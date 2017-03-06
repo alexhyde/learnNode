8cd8b15546db4c31262002a0b043375c5cc9a753
Alex Hyde <ahyde@stepnetworks.com>
2017-03-06 15:03:22 -0500

    Added Express to project and some basic routing with express

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-var test = 'Hello World';
-console.log(test);
+var express = require('express'); //requrie the express packages
+var app = express(); // start express
+
+var port = 5000; //port for express to listen on
+
+app.get('/', function(req,res){
+	res.send('Hello World!');
+});
+
+app.get('/routing', function(req,res){
+	res.send('Hello Routing World!');
+});
+
+app.listen(port,function(err){
+	console.log('The server is running on port: ' + port);
+});
